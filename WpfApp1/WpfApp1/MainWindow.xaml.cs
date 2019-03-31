c3770e5035ce17b7aea89ac1fda92d21537cd38c
RuLe0G <khlebnikov2015@yandex.ru>
2019-03-31 14:56:38 +0700

    SB

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -12,17 +12,46 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace Data_base
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+
+
+    public class CTest { public int Number { get; set; } public string FIO { get; set; } public int Phy { get; set; } public int Math { get; set; } }
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+
+        }
+
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            
+            dlg.ShowDialog();
+
+            string db_name = dlg.FileName;
+
+            SQLiteConnection m_dbConnection; m_dbConnection = new SQLiteConnection("Data Source=" + db_name + ";Version=3;");
+            m_dbConnection.Open();
+
+            string sql = "SELECT * FROM Tabl ORDER BY Number";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            var data = new CTest { Number = int.Parse(reader["Number"].ToString()), FIO = reader["FIO"].ToString(), Phy = int.Parse(reader["Phy"].ToString()), Math = int.Parse(reader["Math"].ToString()) };
+            datgrid.Items.Add(data);
+            m_dbConnection.Close(); 
         }
     }
 }
