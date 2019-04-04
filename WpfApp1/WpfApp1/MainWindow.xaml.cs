72f4b804554f409d9284eb013e9b4258ec984530
RuLe0G <khlebnikov2015@yandex.ru>
2019-04-04 07:19:45 +0700

    Tst

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SQLite;
 
+
 namespace Data_base
 {
     /// <summary>
@@ -29,29 +30,41 @@ namespace Data_base
         public MainWindow()
         {
             InitializeComponent();
-
-
         }
 
-        
+        string db_name;
+        SQLiteConnection m_dbConnection;
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            
+
             dlg.ShowDialog();
 
             string db_name = dlg.FileName;
 
-            SQLiteConnection m_dbConnection; m_dbConnection = new SQLiteConnection("Data Source=" + db_name + ";Version=3;");
+            m_dbConnection = new SQLiteConnection("Data Source=" + db_name + ";Version=3;");
             m_dbConnection.Open();
 
             string sql = "SELECT * FROM Tabl ORDER BY Number";
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
             SQLiteDataReader reader = command.ExecuteReader();
-            var data = new CTest { Number = int.Parse(reader["Number"].ToString()), FIO = reader["FIO"].ToString(), Phy = int.Parse(reader["Phy"].ToString()), Math = int.Parse(reader["Math"].ToString()) };
-            datgrid.Items.Add(data);
-            m_dbConnection.Close(); 
+            while (reader.Read())
+            {
+                var data = new CTest { Number = int.Parse(reader["Number"].ToString()), FIO = reader["FIO"].ToString(), Phy = int.Parse(reader["Phy"].ToString()), Math = int.Parse(reader["Math"].ToString()) };
+                datgrid.Items.Add(data);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            string sql = "INSERT INTO Tabl (Number,FIO,Phy,Math) VALUES (" + Numb.Text.ToString() + "," + FIO.Text.ToString() + "," + Phy.Text.ToString() + "," + Math.Text.ToString() + ")";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+        }
         }
     }
-}
+
